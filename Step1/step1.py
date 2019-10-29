5d69e5cc0af0786a363d54f3b335e2639dc89e8e
eventia <eventia@gmail.com>
2019-10-29 18:04:20 +0900

    first simple selenium example

--- Step1/step1.py
+++ Step1/step1.py
@@ -1,1 +1,8 @@
-print('Hello')
+from selenium import webdriver
+
+path = "C:\\Utils\\chromedriver.exe"
+driver = webdriver.Chrome(path)
+driver.get('https://www.google.com')
+search_box=driver.find_element_by_name('q')
+search_box.send_keys('처음코딩')
+search_box.submit()
