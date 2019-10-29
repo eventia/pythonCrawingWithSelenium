cdacb11a798cf3696076f41603c41c9495034fd3
eventia <eventia@gmail.com>
2019-10-29 19:07:51 +0900

    first simple selenium example

--- Step1/step2.py
+++ Step1/step2.py
@@ -13,8 +13,6 @@ driver.implicitly_wait(3)    #3초 기다림
 
 # 2. 네이버로그인
 driver.get('https://nid.naver.com/nidlogin.login')
-id = 'eventia'
-pw = 'lnv17A1801sk#'
 driver.execute_script("document.getElementsByName('id')[0].value=\'" + id + "\'")
 # time.sleep(1)
 driver.execute_script("document.getElementsByName('pw')[0].value=\'" + pw + "\'")
@@ -22,5 +20,5 @@ driver.execute_script("document.getElementsByName('pw')[0].value=\'" + pw + "\'"
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
 # 아래코드는 현재 작동되지 않음
-# driver.find_element_by_name('id').send_keys('eventia')
-# driver.find_element_by_name('pw').send_keys('lnv17A1801sk#')
+# driver.find_element_by_name('id').send_keys('id')
+# driver.find_element_by_name('pw').send_keys('pw')
